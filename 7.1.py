3604526a071d19cb58d2d9ec8c6f3d23b477ad91
ruslan <voodooley83@gmail.com>
2023-11-08 17:25:19 +0300

    7

---
+++ 7.1.py
@@ -0,0 +1,6 @@
+string = input('Введите строку => ')
+
+if string == string[::-1]:
+    print('yes')
+else:
+    print('no')
---
+++ 7.2.py
@@ -0,0 +1,4 @@
+string = input('Введите строку => ')
+new_string = ' '.join(string.split())
+
+print(new_string)
